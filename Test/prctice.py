df6469d47bf0202addb48e61963d8f8ff0e8bb94
Essilia <1280550032@qq.com>
2021-02-22 11:15:17 +0800

    第一次提交

---
+++ Test/prctice.py
@@ -0,0 +1,26 @@
+# name=["一一","二二","三三"]
+# socre=["99","100","98"]
+# num=0
+# name1=input("请输入姓名：")
+# for i in range(3):
+#     if i == num and name1 == name[num]:
+#         print(name1+"的成绩是："+socre[num])
+#     num=num+1
+
+# d = {"一一":"99","二二":"100","三三":"98"}
+# name2 = input("请输入姓名：")
+# # if name2 in d:
+# #     print(d[name2])
+# # else:
+# #     print(d.get(name2,"查询的用户不存在"))
+# if name2 in d:
+#     d.pop(name2)
+#     print("删除成功！"+"\n"+str(d))
+
+# s=set([1,2,3,[1,2,3]])
+# print(s)
+
+l=[1,2,3]
+l.insert(1,7)
+print(abs(-100))
+print(max(1,2,3))
